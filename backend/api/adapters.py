9d273cd8e32155622320504271eb5fbcf258e625
b-walls <brendan.walls22@gmail.com>
2026-04-17 00:56:54 -0400

    feat: allauth adapter

--- backend/api/adapters.py
+++ backend/api/adapters.py
@@ -1,4 +1,19 @@
+from allauth.account.adapter import DefaultAccountAdapter
 from allauth.headless.adapter import DefaultHeadlessAdapter
+
+
+# class CustomAccountAdapter(DefaultAccountAdapter):
+#     def save_user(self, request, user, form, commit=True):
+#         user = super().save_user(request, user, form, commit=commit)
+#         if commit:
+#             from api.models import Category, Budget, UserSettings, CATEGORY_CHOICES
+#             UserSettings.objects.get_or_create(user=user)
+#             for item in CATEGORY_CHOICES:
+#                 category, created = Category.objects.get_or_create(name=item, user=user)
+#                 if created:
+#                     Budget.objects.create(user=user, category=category, limit=0)
+#         return user
+
 
 class CustomHeadlessAdapter(DefaultHeadlessAdapter):
     def serialize_user(self, user):
